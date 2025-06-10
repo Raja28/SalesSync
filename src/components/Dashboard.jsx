e31f1d2b9349de122319c8d7c2267d3b60c30198
Raja28 <rajadavid03@gmail.com>
2025-06-10 20:14:08 +0530

    fix: removed unwanted border

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -73,7 +73,7 @@ export function Dashboard() {
     return (
         <>
             <section className="container mt-4">
-                <div className="border d-flex justify-content-between align-items-center mb-4">
+                <div className=" d-flex justify-content-between align-items-center mb-4">
                     <h2 className="mx-auto">Product List</h2>
                     {/* <button className="btn btn-sm btn-warning fw-semibold">Add Product</button> */}
                 </div>
